a2b4fbecdd962ea3c5c351d91068a37103f86903
Nitesh Tosniwal <nktosni@gmail.com>
2019-03-28 22:07:03 +0530

    user controllers and routes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
-
+const formidable = require('formidable')
+const fs = require('fs')
+const _ = require('lodash')
 
 const UserModel = require('../models').User;
 
@@ -98,13 +100,16 @@ const user_login = (req, res, next) => {
 }
 
 const user_remove = (req, res, next) => {
+  // console.log(req.userData.userId)
   UserModel.remove({
-      _id: req.params.id
+      _id: req.userData.userId
     })
     .exec()
-    .then(res => {
-      res.status(200).json({
-        messgae: 'user present, need to be deleted'
+    .then(user => {
+      return res.json({
+        status: 'success',
+        deletedUser: user,
+        message: 'user deleted'
       })
     })
     .catch(err => {
@@ -115,13 +120,81 @@ const user_remove = (req, res, next) => {
     })
 }
 
+const user_update = (req, res, next) => {
+  const id = req.userData.userId
+  UserModel
+    .findById(id)
+    .exec()
+    .then(user => {
+      req.profile = user
+      // console.log(req.profile)
+      let form = new formidable.IncomingForm()
+      form.keepExtensions = true
+      form.parse(req, (err, fields, files) => {
+        if(err) {
+          res.status(500).json({
+            error: err
+          })
+        }
+        user = _.extend(user, fields)
+        user.updatedDate = Date.now()
+        if(files.photo){
+          user.photo.data = fs.readFileSync(files.photo.path)
+          user.photo.contentType = files.photo.type
+        }
+        user.save((err, result) => {
+          if (err) {
+            return res.status(400).json({
+              error: errorHandler.getErrorMessage(err)
+            })
+          }
+          res.json(user)
+        })
+      })
+
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+}
+
+const photo = (req, res, next) => {
+  const id = req.userData.userId
+  UserModel
+    .findById(id)
+    .exec()
+    .then(user => {
+      if(user.photo.data) {
+        res.set("Content-Type", user.photo.contentType)
+        return res.send(user.photo.data)
+      }
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+}
+
+const defaultPhoto = (req, res) => {
+  return res.sendFile(process.cwd()+profileImage)
+}
+
 const user_findById = (req, res, next) => {
   UserModel
-    .find({_id: req.params.id})
+    .find({
+      _id: req.params.id
+    })
     .populate('following', '_id name')
     .populate('followers', '_id name')
     .exec()
     .then(user => {
+      user[0].password = undefined
       console.log(user)
       res.json(user)
     })
@@ -133,9 +206,126 @@ const user_findById = (req, res, next) => {
     })
 }
 
+const addFollowing = (req, res, next) => {
+  UserModel
+    .findByIdAndUpdate(req.userData.userId, {
+      $push: {
+        following: req.body.followId
+      }
+    }, (err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        })
+      }
+      console.log(result)
+      next()
+    })
+}
+
+const addFollower = (req, res, next) => {
+  UserModel
+    .findByIdAndUpdate(req.body.followId, {
+      $push: {
+        followers: req.userData.userId
+      }
+    }, {
+      new: true
+    })
+    .populate('following', '_id name')
+    .populate('followers', '_id name')
+    .exec()
+    .then(result => {
+      result.password = undefined
+      res.json(result)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+
+}
+
+const removeFollowing = (req, res, next) => {
+  UserModel
+    .findByIdAndUpdate(req.userData.userId, {
+      $pull: {
+        following: req.body.unfollowId
+      }
+    }, (err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        })
+      }
+      next()
+    })
+}
+const removeFollower = (req, res) => {
+  UserModel
+    .findByIdAndUpdate(req.body.unfollowId, {
+      $pull: {
+        followers: req.userData.userId
+      }
+    }, {
+      new: true
+    })
+    .populate('following', '_id name')
+    .populate('followers', '_id name')
+    .exec((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        })
+      }
+      result.password = undefined
+      res.json(result)
+    })
+}
+
+const findPeople = (req, res) => {
+  const id = req.userData.userId
+  UserModel
+    .findById(id)
+    .exec()
+    .then(user => {
+      let following = user.following
+      following.push(user._id)
+
+      UserModel
+        .find({ _id: { $nin : following } }, (err, users) => {
+          if (err) {
+            return res.status(400).json({
+              error: err
+            })
+          }
+          res.json(users)
+        }).select('name')
+
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+}
+
+
 module.exports = {
   user_signup,
   user_login,
   user_remove,
-  user_findById
+  user_findById,
+  addFollowing,
+  addFollower,
+  removeFollowing,
+  removeFollower,
+  findPeople,
+  // not tested below
+  user_update,
+  photo,
+  defaultPhoto
 }
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -8,6 +8,30 @@ const router = express.Router();
 
 router.post('/login', userController.user_login)
 router.post('/signup', userController.user_signup)
-router.get('/:id',checkAuth, userController.user_findById)
 
+router.get('/findUser/:id', checkAuth, userController.user_findById)
+router.delete('/del_account', checkAuth, userController.user_remove)
+// add following in my list && add follower in the person's list
+router.put('/follow', checkAuth, userController.addFollowing, userController.addFollower)
+router.put('/unfollow', checkAuth, userController.removeFollowing, userController.removeFollower)
+router.get('/update', checkAuth, userController.user_update)
+router.get('/findPeople', checkAuth, userController.findPeople)
 module.exports = router;
+
+// const appendUserToREQ = (req, res, next) => {
+//   UserModel
+//     .findById(req.userData.userId)
+//     .populate('following', '_id name')
+//     .populate('followers', '_id name')
+//     .exec()
+//     .then(user => {
+//       req.profile = user
+//       next()
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({
+//         error: err
+//       })
+//     })
+// }
